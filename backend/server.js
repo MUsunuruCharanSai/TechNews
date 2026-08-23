2eec697045db070028c8c58679af46a3c0447ccf
MUsunuruCharanSai <charansaimusunuru@gmail.com>
2026-08-23 17:57:35 +0530

    connect item and category apis

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const connectDB = require('./config/db');
 const seedAdmin = require('./utils/seedAdmin');
 const authRoutes = require('./routes/authRoutes');
 const articleRoutes = require('./routes/articleRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const itemRoutes = require('./routes/itemRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,6 +18,8 @@ app.use(express.json());
 // routes
 app.use('/api/auth', authRoutes);
 app.use('/api/articles', articleRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/items', itemRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'TechNews API is running' });
