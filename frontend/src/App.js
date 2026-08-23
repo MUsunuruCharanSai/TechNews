8a10a3d18178cce27d498556c9af06feed554919
MUsunuruCharanSai <charansaimusunuru@gmail.com>
2026-08-23 17:58:19 +0530

    add item detail route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ArticleDetail from './pages/ArticleDetail';
+import ItemDetail from './pages/ItemDetail';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
 
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/item/:id" element={<ItemDetail />} />
           <Route path="/article/:id" element={<ArticleDetail />} />
           <Route path="/admin/login" element={<AdminLogin />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
